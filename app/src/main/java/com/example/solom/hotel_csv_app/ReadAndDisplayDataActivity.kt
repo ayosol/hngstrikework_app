c857e6b47d2206aae3b0704feb81c4e974e28092
Maaruf Dauda <mkdauda.work@gmail.com>
2018-11-05 23:24:09 +0100

    Convert ReadAndDisplayActivity to Kotlin

---
+++ app/src/main/java/com/example/solom/hotel_csv_app/ReadAndDisplayDataActivity.kt
@@ -0,0 +1,13 @@
+package com.example.solom.hotel_csv_app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ReadAndDisplayDataActivity : AppCompatActivity() {
+
+    @Override
+    protected fun onCreate(savedInstanceState: Bundle) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_read_and_display_data)
+    }
+}
