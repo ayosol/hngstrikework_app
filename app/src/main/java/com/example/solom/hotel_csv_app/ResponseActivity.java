5803b3b3cfd59e5d5798cf37a2d3cc0474413d8f
ayosol <solomonayodeleo@gmail.com>
2018-11-05 19:40:25 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/solom/hotel_csv_app/ResponseActivity.java
@@ -0,0 +1,13 @@
+package com.example.solom.hotel_csv_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResponseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_response);
+    }
+}
---
+++ app/src/main/java/com/example/solom/hotel_csv_app/UploadFileActivity.java
@@ -0,0 +1,13 @@
+package com.example.solom.hotel_csv_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UploadFileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_file);
+    }
+}
