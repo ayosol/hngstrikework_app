8604bf1603e48c04eed0e5dd70fc35826a65c34d
Ismail Nurudeen <ibrightstar247@gmail.com>
2018-11-08 16:28:05 +0100

    Removed irrelevant old implementation code using ListView

--- app/src/main/java/com/example/solom/hotel_csv_app/MainActivity.java
+++ app/src/main/java/com/example/solom/hotel_csv_app/MainActivity.java
@@ -3,72 +3,48 @@ package com.example.solom.hotel_csv_app;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Parcelable;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
-
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-import android.widget.ListView;
-
-import java.io.InputStream;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ItemArrayAdapter itemArrayAdapter;
-
     private static final int SMS_PERMISSION_CODE = 102;
-    private static final String TAG ="PERMISSION" ;
-  @BindView(R.id.test)
-    TextView test;
+    private static final String TAG = "PERMISSION";
+    @BindView(R.id.test_btn)
+    Button test_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        test.setOnClickListener(new View.OnClickListener() {
+        test_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(MainActivity.this,ResponseActivity.class);
+                Intent intent = new Intent(MainActivity.this, ResponseActivity.class);
                 startActivity(intent);
             }
         });
 
-        ListView listView = findViewById(R.id.listview);
-        itemArrayAdapter = new ItemArrayAdapter(getApplicationContext(), R.layout.single_list_item);
-
-        Parcelable state = listView.onSaveInstanceState();
-        listView.setAdapter(itemArrayAdapter);
-        listView.onRestoreInstanceState(state);
-
-        InputStream inputStream = getResources().openRawResource(R.raw.stats);
-        ReadAndDisplayDataActivity_Java csv = new ReadAndDisplayDataActivity_Java(inputStream);
-        List<String[]> infoList = csv.read();
-
-        for(String [] info : infoList){
-            itemArrayAdapter.add(info);
-        }
-
-        if (!hasSendSmsPermission()){
+        if (!hasSendSmsPermission()) {
             requestSendSmsPermission();
         }
     }
-
-
-
 
     private boolean hasSendSmsPermission() {
         return ContextCompat.checkSelfPermission(this,
                 Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED;
     }
+
     private void requestSendSmsPermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) {
             Log.d(TAG, "shouldShowRequestPermissionRationale(), no permission requested");
