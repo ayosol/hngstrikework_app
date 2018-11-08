76e0701716aa0655e06a9458c219b28075c03905
Ismail Nurudeen <ibrightstar247@gmail.com>
2018-11-08 17:40:45 +0100

    Read csv file path in MainActivity and pass it to ReadAndDisplayActivity,also changed the app name and organized manifest

--- app/src/main/java/com/example/solom/hotel_csv_app/MainActivity.java
+++ app/src/main/java/com/example/solom/hotel_csv_app/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,21 +18,25 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static String EXTRAS_CSV_PATH_NAME="MainActivity.PathHolder";
     private static final int SMS_PERMISSION_CODE = 102;
     private static final String TAG = "PERMISSION";
-    @BindView(R.id.test_btn)
-    Button test_btn;
-
+    private static final int CSV_UPLOAD_REQUEST_CODE = 107;
+    @BindView(R.id.readcsvfile)
+    Button readCsvFile;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        test_btn.setOnClickListener(new View.OnClickListener() {
+
+        readCsvFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ResponseActivity.class);
-                startActivity(intent);
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");
+                startActivityForResult(intent, CSV_UPLOAD_REQUEST_CODE);
             }
         });
 
@@ -53,5 +58,21 @@ public class MainActivity extends AppCompatActivity {
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS},
                 SMS_PERMISSION_CODE);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case CSV_UPLOAD_REQUEST_CODE:
+                if (resultCode == RESULT_OK) {
+                    String PathHolder = data.getData().getPath();
+                    Toast.makeText(getApplicationContext(), PathHolder, Toast.LENGTH_LONG).show();
+                    Intent readAndDisplayIntent=new Intent(MainActivity.this, ReadAndDisplayActivity.class);
+                    readAndDisplayIntent.putExtra(EXTRAS_CSV_PATH_NAME,PathHolder);
+                    startActivity(readAndDisplayIntent);
+                }
+                break;
+        }
+    }
+
 
 }
