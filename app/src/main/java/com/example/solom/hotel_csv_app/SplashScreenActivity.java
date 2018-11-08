29a520368580c7266fe0e7892ca763756000d0ba
Ismail Nurudeen <ibrightstar247@gmail.com>
2018-11-08 17:49:08 +0100

    Created a layout file for the splash activity and also added a 3sec delay to the splash screen

--- app/src/main/java/com/example/solom/hotel_csv_app/SplashScreenActivity.java
+++ app/src/main/java/com/example/solom/hotel_csv_app/SplashScreenActivity.java
@@ -1,21 +1,23 @@
 package com.example.solom.hotel_csv_app;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-/**
- * @author Egahi
- */
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
