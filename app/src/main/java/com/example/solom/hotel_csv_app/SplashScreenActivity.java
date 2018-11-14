0437118c1c5006439b392044fe054e7b87a83176
Ismail Nurudeen <ibrightstar247@gmail.com>
2018-11-14 19:46:07 +0100

    Implemented the TapTargetView for Upload Button and SMS sending button.

--- app/src/main/java/com/example/solom/hotel_csv_app/SplashScreenActivity.java
+++ app/src/main/java/com/example/solom/hotel_csv_app/SplashScreenActivity.java
@@ -4,12 +4,8 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Window;
-
-import java.util.Objects;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
